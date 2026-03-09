2510fa4d89e7ae6d69dbbe506a7bb5ec7c2c20de
Aaron Yourk <ayourk@gmail.com>
2026-03-09 10:41:04 -0600

    Fix ARM64 static linker machine type mismatch
    Chainload the custom toolchain that adds /MACHINE:ARM64 flags to the
    static linker. Without this, lib.exe defaults to x64 on cross-compile,
    causing LNK1112 errors in libgit2 and other libraries.
    
    Signed-off-by: Aaron Yourk <ayourk@gmail.com>

--- vcpkg-custom-triplets/arm64-windows-static-md.cmake
+++ vcpkg-custom-triplets/arm64-windows-static-md.cmake
@@ -11,3 +11,6 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_BUILD_TYPE release)
 
+# Chainload custom toolchain to add /MACHINE:ARM64 for static linker
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/arm64-windows-toolchain.cmake")
+
