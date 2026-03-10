f6a1edb02e27c8b7dbc493c9c266dce9f4e52921
Aaron Yourk <ayourk@gmail.com>
2026-03-09 21:36:04 -0600

    Remove CMAKE_AR wrapper to test fontconfig build
    Remove the lib.exe wrapper to test if fontconfig builds without it.
    This will likely cause libgit2 to fail with LNK1112, but will confirm
    if the wrapper was causing the meson csr issue.
    
    Signed-off-by: Aaron Yourk <ayourk@gmail.com>

--- vcpkg-custom-triplets/arm64-windows-static-md.cmake
+++ vcpkg-custom-triplets/arm64-windows-static-md.cmake
@@ -8,12 +8,3 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_BUILD_TYPE release)
 
-# Use lib.exe wrapper that adds /MACHINE:ARM64
-# The wrapper path is passed via LIB_ARM64_WRAPPER environment variable
-set(VCPKG_ENV_PASSTHROUGH LIB_ARM64_WRAPPER)
-if(DEFINED ENV{LIB_ARM64_WRAPPER})
-    set(VCPKG_CMAKE_CONFIGURE_OPTIONS
-        "-DCMAKE_AR=$ENV{LIB_ARM64_WRAPPER}"
-    )
-endif()
-
